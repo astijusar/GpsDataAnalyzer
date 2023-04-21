cf231f4f1710b8239963a961e90e68f12d286b3c
Astijus Arimavičius <astijus.arimavicius@gmail.com>
2023-04-21 19:37:53 +0300

    Add GpsData model

---
+++ GpsDataAnalyzer/Models/GpsData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GpsDataAnalyzer.Models
+{
+    public class GpsData
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime GpsTime { get; set; }
+        public int Speed { get; set; }
+        public int Angle { get; set; }
+        public int Altitude { get; set; }
+        public int Satelites { get; set; }
+
+        public GpsData(double latitude, double longitude, DateTime gpsTime, int speed, int angle, int altitude, int satelites)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            GpsTime = gpsTime;
+            Speed = speed;
+            Angle = angle;
+            Altitude = altitude;
+            Satelites = satelites;
+        }
+    }
+}
